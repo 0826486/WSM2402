bdc772068a2b971f516f379c9a5cf84a95f0d0e6
호정 <w2314@e-mirim.hs.kr>
2024-10-29 15:38:25 +0900

    dormitory 세탁기 예약 현황표에 날짜 추가

--- 240514_nav/js/dormitory.js
+++ 240514_nav/js/dormitory.js
@@ -18,7 +18,9 @@ const setPage = (page) => {
     }
 
     // selection 칠하기
-    selectionItemDivs[page-1].classList.add("select-menu");
+    if (page != 4){     // 세탁기 예약 현황표는 selection이 없음
+        selectionItemDivs[page-1].classList.add("select-menu");
+    }
 
     // clear pageDiv
     pageDivs.forEach(pageDiv => {
